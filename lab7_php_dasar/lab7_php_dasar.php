cfc5d9cf1942d3762b35d16bcb9ab0be25baf213
SandyRamadhan28 <ramadhansandy388@gmail.com>
2023-11-13 11:25:05 +0700

    upload file

---
+++ lab7_php_dasar/lab7_php_dasar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Dasar</title>
+</head>
+<body>
+    <h1>Belajar PHP Dasar</h1>
+    <?php
+    echo "Hello World";
+    ?>
+    <!-- Variabel PHP -->
+    <h2>Menggunakan Variabel</h2>
+    <?php
+        $nim = "312210633";
+        $nama = 'Sandy Ramadhan';
+        echo "NIM : " . $nim . "<br>";
+        echo "Nama : $nama";
+    ?>
+</body>
+</html>
